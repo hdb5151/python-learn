f30726c9253b789e89b5f0cc9dc37b83350098c3
hdb5151 <hdb5151@163.com>
2022-07-26 00:10:48 +0800

    home_1

--- theTest.py
+++ theTest.py
@@ -1,15 +1,9 @@
-import unittest
-import torch
+# import unittest
+# import torch
 
-x = torch.rand(5, 5, requires_grad=True)
-print("x==", x)
+def add(a,b):
+      c=a+b
+      return c
 
-y = torch.rand(5, 5, requires_grad=True)
-print("y==", y)
+print(add(d=22,a=23))
 
-# z = x.add(y)
-z = torch.sum(x + y)
-print("z==", z)
-
-z.backward()
-print("x.grad==", x.grad)
